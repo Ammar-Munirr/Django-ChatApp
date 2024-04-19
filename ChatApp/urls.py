36cbbf92f6a4e4457e1eab7fd6070b01d7af0d01
Ammar Munir <munirammar0@gmail.com>
2024-04-19 11:53:19 +0500

    Setup views and templates logic.

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.CreateRoom, name="create-room"),
+    path("<str:room_name>/<str:username>/", views.MessageView, name="room"),
+]
